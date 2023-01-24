9b86062e37e378bf0e0fe92eef59912b2eb1499c
nsandarma <nsandarma@gmail.com>
2023-01-25 01:21:04 +0700

    add decorator

--- main.py
+++ main.py
@@ -2,10 +2,36 @@ from flask import Flask,request,make_response,jsonify
 from flask_restful import Api,Resource
 from datetime import datetime, timedelta
 import jwt
+from functools import wraps
+
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SECRET_KEY'] = 'fasisme'
+
+def token_required(func):
+    @wraps(func)
+    def decorator(*args,**kwargs):
+        token = request.args.get('token')
+        if not token:
+            return make_response(
+                        jsonify(
+                                {"msg":"not found token"}
+                            )
+                        ,404
+                    )
+        try:
+            output = jwt.decode(token,app.config['SECRET_KEY'],
+                                algorithms=['HS256'])
+            return make_response(jsonify({
+                    "output":output
+                }),200)
+
+        except Exception as e:
+            return make_response(jsonify({
+                    "msg":f'{e}'
+                }),404)
+    return decorator
 
 class Login(Resource):
     def post(self):
@@ -16,7 +42,7 @@ class Login(Resource):
             token = jwt.encode(
                         {
                             "username":username,
-                            "exp":datetime.utcnow()+timedelta()
+                            "exp":datetime.utcnow()+timedelta(minutes=2)
                         },app.config['SECRET_KEY'],algorithm="HS256"
 
                     )
@@ -30,7 +56,15 @@ class Login(Resource):
                 "status": 0
             })
 
-api.add_resource(Login,"/api/login")
+class Home(Resource):
+    @token_required
+    def get(self):
+        return jsonify({
+                "msg":"This is Home Page"
+            })
 
+
+api.add_resource(Login,"/api/login")
+api.add_resource(Home,'/api')
 if __name__ == "__main__":
     app.run(debug=True)
